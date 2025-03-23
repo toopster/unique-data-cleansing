cc247841771140ceedf8fbaaa4f2abc272d0507a
James Toop <jj.toop@gmail.com>
2025-03-23 17:08:09 +0000

    Initial commit

---
+++ pandas-example.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df = pd.read_csv("countries.csv")
+
+print(df.tail())
---
+++ testing.py
@@ -0,0 +1,4 @@
+first=input("first: ")
+second=input("second: ")
+sum=float(first)+float(second)
+print("sum: "+str(sum))
