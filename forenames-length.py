c2c216b799cf89127ca5a76a7557d475c206ec1e
James Toop <jj.toop@gmail.com>
2025-04-27 12:28:16 +0100

    More code to cleanse the forenames
    Identifying individuals that have died
    Identifying data that is longer than expected and will require manual validation

---
+++ forenames-length.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+long_first_name = "Happy to be contacted by other parents"
+
+print(len(long_first_name))
+long_real_name = "Carrie-Anne"
+
+print(len(long_real_name))
--- forenames.py
+++ forenames.py
@@ -4,6 +4,8 @@ import pandas as pd
 # Load the data into a dataframe
 df = pd.read_csv("forenames.csv")
 
+df["FirstName"] = df["FirstName"].astype(str)
+
 cleaned_data = []
 
 # Loop through dataframe finding two names
@@ -12,10 +14,10 @@ for index, row in df.iterrows():
     member_id = row["MemberID"]
 
     # Check for &
-    if str(row["FirstName"]).find(" & ") != -1:
+    if row["FirstName"].find(" & ") != -1:
 
-        person_1 = str(row["FirstName"])[:str(row["FirstName"]).find(" & ")]
-        person_2 = str(row["FirstName"])[str(row["FirstName"]).find(" & ") + len(" & "):len(str(row["FirstName"]))]
+        person_1 = row["FirstName"][:row["FirstName"].find(" & ")]
+        person_2 = row["FirstName"][row["FirstName"].find(" & ") + len(" & "):len(row["FirstName"])]
 
         cleaned_data.append([member_id, person_1])
         cleaned_data.append([member_id, person_2])
@@ -23,8 +25,8 @@ for index, row in df.iterrows():
     # Check for and
     elif str(row["FirstName"]).find(" and ") != -1:
 
-        person_1 = str(row["FirstName"])[:str(row["FirstName"]).find(" and ")]
-        person_2 = str(row["FirstName"])[str(row["FirstName"]).find(" and ") + len(" and "):len(str(row["FirstName"]))]
+        person_1 = row["FirstName"][:row["FirstName"].find(" and ")]
+        person_2 = row["FirstName"][row["FirstName"].find(" and ") + len(" and "):len(row["FirstName"])]
 
         cleaned_data.append([member_id, person_1])
         cleaned_data.append([member_id, person_2])
@@ -35,4 +37,22 @@ for index, row in df.iterrows():
 
 cleaned_df = pd.DataFrame(cleaned_data, columns=["MemberID", "FirstName"])
 
-print(cleaned_df.head(40))
+                                                                                                                                                                                                                                                                                               
+
+cleaned_df["FirstNameLength"] = cleaned_df["FirstName"].str.len()
+
+# View final dataframe 
+# print(cleaned_df)
+
+# Now filter out those rows in the Pandas dataframe that are greater than 14 and put them into a new dataframe
+filtered_cleaned_df = cleaned_df[cleaned_df['FirstNameLength'] > 14]
+
+# print(filtered_cleaned_df)
+
+
+# Now filter out those rows in the Pandas dataframe that are greater than 14 from the original dataframe
+filtered_cleaned_df2 = cleaned_df[cleaned_df['FirstNameLength'] < 14]
+# print(filtered_cleaned_df2)
+
+deceased_df = cleaned_df[cleaned_df['FirstName'].str.contains("died|DIED")]
+print(deceased_df)
